5887ba5ed4a379ca61b1920cc3a7c731f8021071
Maysoon Tabali <mtab03@me.com>
2020-04-21 20:27:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -21,11 +21,11 @@ end
 def turn_count(board)
   turns = 0
   board.each do |space|
-  if space =="X" || space =="O"  
+  if space =="X" || space =="O"
     turns = += 1
-  end 
-  return turns 
-end 
+  end
+  return turns
+end
 
 def current_player(board)
   turns = turn_count(board)
